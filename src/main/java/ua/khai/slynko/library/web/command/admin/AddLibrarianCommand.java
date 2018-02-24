c3f81ae13e30288d43b92a522b61ebf381807818
slynko <oleksandr.o.slynko@gmail.com>
2018-02-24 09:42:04 +0200

    useless logging and comments removed

--- src/main/java/ua/khai/slynko/library/web/command/admin/AddLibrarianCommand.java
+++ src/main/java/ua/khai/slynko/library/web/command/admin/AddLibrarianCommand.java
@@ -36,53 +36,35 @@ public class AddLibrarianCommand extends Command {
 	@Override
 	public String execute(HttpServletRequest request, HttpServletResponse response)
 			throws IOException, ServletException, AppException {
-		LOG.debug("Command starts");
-		// obtain session from request
 		HttpSession session = request.getSession();
-		// init address to forward/redirect
-		String address = Path.PAGE_ERROR_PAGE;
-		LOG.trace("Forward address --> " + address);
-		// check if input data is valid
+		String address;
 		if (!inputDataIsValid(request)) {
 			address = Path.PAGE_ADD_LIBRARIAN;
 			LOG.trace("Forward address --> " + address);
 		} else {
-			// create user and initialize it's fields
 			User user = new User();
-			user.setLogin((String) request.getParameter("login"));
+			user.setLogin(request.getParameter("login"));
 			try {
-				user.setPassword(Password.hash((String) request.getParameter("password1")));
+				user.setPassword(Password.hash(request.getParameter("password1")));
 			} catch (NoSuchAlgorithmException e) {
 				throw new AppException("NoSuchAlgorithmException", e);
 			}
-			user.setFirstName((String) request.getParameter("firstName"));
-			user.setLastName((String) request.getParameter("lastName"));
-			user.setEmail((String) request.getParameter("email"));
+			user.setFirstName(request.getParameter("firstName"));
+			user.setLastName(request.getParameter("lastName"));
+			user.setEmail(request.getParameter("email"));
 			user.setRoleId(Role.LIBRARIAN.getValue());
-			LOG.info("User " + user + " is going to be inserted into database");
-			// create new user in db
 			DBManager.getInstance().createUser(user);
-			LOG.trace("Inserted in DB: user --> " + user);
-			// set send redirect ture into request
 			request.setAttribute("sendRedirect", true);
-			// update address to forward/redirect
 			address = Path.PAGE_LOGIN_REDERECT;
-			LOG.trace("Redirect address --> " + address);
-			// set redirect page into session
 			session.setAttribute("redirectPage", Path.COMMAND_LIST_LIBRARIANS);
-			LOG.trace("Redirect page --> " + Path.COMMAND_LIST_LIBRARIANS);
-			// set librarian add is successful into session
 			session.setAttribute("librarianAddIsSuccessful", true);
-			LOG.trace("librarianAddIsSuccessful --> " + true);
 		}
-		LOG.debug("Command finished");
 		return address;
 	}
 
 	private boolean inputDataIsValid(HttpServletRequest request) throws IOException, ServletException, AppException {
 		boolean isValid = true;
 		DBManager dbManager = DBManager.getInstance();
-		// init fields for validation from request
 		String firstName = request.getParameter("firstName");
 		String lastName = request.getParameter("lastName");
 		String login = request.getParameter("login");
@@ -93,9 +75,7 @@ public class AddLibrarianCommand extends Command {
 		Pattern emailPattern = Pattern.compile("^[a-zA-Z][a-zA-Z0-9\\.-]*\\@[a-zA-Z]+\\.[a-zA-Z]+$");
 		HttpSession session = request.getSession();
 		
-		// obtain resource bundle by current locale
 		String currentLocale = (String) session.getAttribute("currentLocale");
-		LOG.debug("Locale --> " + currentLocale);
 		ResourceBundle rb;
 		if (currentLocale == null) {
 			rb = ResourceBundle.getBundle("resources", Locale.getDefault());
@@ -106,15 +86,11 @@ public class AddLibrarianCommand extends Command {
 		Captcha captcha = (Captcha) session.getAttribute(Captcha.NAME);
 		String capthaAnswer = request.getParameter("capthaAnswer");
 
-		LOG.trace("Validation starts");
-
-		// check if fields are filled
 		if (login == null && email == null && firstName == null && lastName == null) {
 			request.setAttribute("fillInMessage", rb.getString("signup.fieldsAreNotFilled"));
 			isValid = false;
 			return isValid;
 		}
-		// first name validation
 		if (firstName == null || firstName.length() < 1) {
 			request.setAttribute("firstNameMessage", rb.getString("signup.firstNameTooShort"));
 			isValid = false;
@@ -124,7 +100,6 @@ public class AddLibrarianCommand extends Command {
 		} else {
 			request.setAttribute("firstName", firstName);
 		}
-		// last name validation
 		if (lastName == null || lastName.length() < 1) {
 			request.setAttribute("lastNameMessage", rb.getString("signup.lastNameTooShort"));
 			isValid = false;
@@ -134,7 +109,6 @@ public class AddLibrarianCommand extends Command {
 		} else {
 			request.setAttribute("lastName", lastName);
 		}
-		// login validation
 		String loginMessage = "loginMessage";
 		if (login == null || login.length() < 3) {
 			request.setAttribute(loginMessage, rb.getString("signup.loginTooShort"));
@@ -151,7 +125,6 @@ public class AddLibrarianCommand extends Command {
 		} else {
 			request.setAttribute("login", login);
 		}
-		// email validation
 		String emailMessage = "emailMessage";
 		if (email == null || !emailPattern.matcher(email).matches()) {
 			request.setAttribute(emailMessage, rb.getString("signup.emailIsNotValid"));
@@ -171,7 +144,6 @@ public class AddLibrarianCommand extends Command {
 		} else {
 			request.setAttribute("email", email);
 		}
-		// password validation
 		if (password1 == null || password1.length() < 5) {
 			request.setAttribute("passwordMessage", rb.getString("signup.passwordTooShort"));
 			isValid = false;
@@ -182,12 +154,10 @@ public class AddLibrarianCommand extends Command {
 			request.setAttribute("passwordMessage", rb.getString("signup.passwordsAreNotEqual"));
 			isValid = false;
 		}
-		// captcha validation
 		if (captcha != null && (capthaAnswer == null || !captcha.isCorrect(capthaAnswer))) {
 			request.setAttribute("captchaMessage", rb.getString("signup.captchaIsNotCorrect"));
 			isValid = false;
 		}
-		LOG.trace("Validation end");
 		return isValid;
 	}
 }
