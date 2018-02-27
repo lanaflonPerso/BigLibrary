e6256a7a564fba24f42a599eacb35948e79ee14d
Aleksandr Slynko <oleksandr.o.slynko@gmail.com>
2018-02-27 10:26:34 +0200

    NoCommand refactoring #3

--- src/main/java/ua/khai/slynko/library/web/command/common/NoCommand.java
+++ src/main/java/ua/khai/slynko/library/web/command/common/NoCommand.java
@@ -3,8 +3,6 @@ package ua.khai.slynko.library.web.command.common;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.log4j.Logger;
-
 import ua.khai.slynko.library.Path;
 import ua.khai.slynko.library.web.abstractCommand.Command;
 
@@ -16,20 +14,9 @@ import ua.khai.slynko.library.web.abstractCommand.Command;
  */
 public class NoCommand extends Command {
 
-	private static final long serialVersionUID = -2785976616686657267L;
-
-	private static final Logger LOG = Logger.getLogger(NoCommand.class);
-
 	@Override
 	public String execute(HttpServletRequest request, HttpServletResponse response) {
-		LOG.debug("Command starts");
-
-		String errorMessage = "No such command";
-		request.setAttribute("errorMessage", errorMessage);
-		LOG.error("Set the request attribute: errorMessage --> " + errorMessage);
-
-		LOG.debug("Command finished");
+		request.setAttribute("errorMessage", "No such command");
 		return Path.PAGE_ERROR_PAGE;
 	}
-
 }
