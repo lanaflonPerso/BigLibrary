e001fcaa8cdca223fee1446a9799671203409180
Aleksandr Slynko <oleksandr.o.slynko@gmail.com>
2018-02-27 10:31:54 +0200

    UpdateProficeCommand and ViewSettingsCommand refactored #3

--- src/main/java/ua/khai/slynko/library/web/command/common/UpdateProfileCommand.java
+++ src/main/java/ua/khai/slynko/library/web/command/common/UpdateProfileCommand.java
@@ -9,12 +9,12 @@ import java.util.Map;
 import java.util.ResourceBundle;
 import java.util.regex.Pattern;
 
-import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.log4j.Logger;
 
 import ua.khai.slynko.library.Path;
@@ -32,7 +32,6 @@ import ua.khai.slynko.library.web.abstractCommand.Command;
  */
 public class UpdateProfileCommand extends Command {
 
-	private static final long serialVersionUID = -3071536593627692473L;
 	private static final Logger LOG = Logger.getLogger(UpdateProfileCommand.class);
 
 	private static final String REQUEST_FIRST_NAME = "firstName";
@@ -47,30 +46,18 @@ public class UpdateProfileCommand extends Command {
 
 	@Override
 	public String execute(HttpServletRequest request, HttpServletResponse response)
-			throws IOException, ServletException, AppException {
-		LOG.debug("Command starts");
-
-		// init address
-		String address = Path.PAGE_ERROR_PAGE;
-		LOG.trace("Forward address --> " + address);
-
-		// check if input data is valid
+			throws IOException, AppException {
+		String address;
 		if (!inputDataIsValid(request)) {
 			address = Path.PAGE_UPDATE_PROFILE;
-			LOG.trace("Forward address --> " + address);
 		} else {
-			// obtain session
 			HttpSession session = request.getSession();
-			// obtain old and new passwords from request
 			String oldPassword = request.getParameter(REQUEST_OLD_PASSWORD_1);
 			String newPassword = request.getParameter(REQUEST_NEW_PASSWORD_1);
-
-			// obtain user from session
 			User user = (User) session.getAttribute(REQUEST_USER);
-			// update user fields
-			user.setLogin((String) request.getParameter(REQUEST_LOGIN));
+			user.setLogin(request.getParameter(REQUEST_LOGIN));
 			try {
-				if (newPassword == null || newPassword.length() == 0) {
+				if (StringUtils.isEmpty(newPassword)) {
 					user.setPassword(Password.hash(oldPassword));
 				} else {
 					user.setPassword(Password.hash(newPassword));
@@ -78,56 +65,33 @@ public class UpdateProfileCommand extends Command {
 			} catch (NoSuchAlgorithmException e) {
 				throw new AppException("NoSuchAlgorithm", e);
 			}
-			user.setFirstName((String) request.getParameter(REQUEST_FIRST_NAME));
-			user.setLastName((String) request.getParameter(REQUEST_LAST_NAME));
-			user.setEmail((String) request.getParameter(REQUEST_EMAIL));
-			LOG.info("User " + user + " is going to be inserted into database");
-			// update user in db
+			user.setFirstName(request.getParameter(REQUEST_FIRST_NAME));
+			user.setLastName(request.getParameter(REQUEST_LAST_NAME));
+			user.setEmail(request.getParameter(REQUEST_EMAIL));
 			DBManager.getInstance().updateUser(user);
-			LOG.trace("Updated in DB: user --> " + user);
-			// set updated user into session
 			session.setAttribute(REQUEST_USER, user);
-			LOG.info("Updated in session user -> " + user);
-			// set user role into session
 			request.setAttribute("sendRedirect", true);
-			// set redirect page
 			address = Path.PAGE_HOME_REDERECT;
-			LOG.trace("Redirect address --> " + address);
-			// set after update redirect page
 			session.setAttribute("redirectPage", Path.COMMAND_SETTINGS);
-			LOG.trace("Redirect page --> " + Path.COMMAND_SETTINGS);
-			// set successful message into session
 			session.setAttribute("profileUpdateIsSuccessful", true);
-			LOG.trace("profileUpdateIsSuccessful --> " + true);
 		}
-		LOG.debug("Command finished");
 		return address;
 	}
 
-	private boolean inputDataIsValid(HttpServletRequest request) throws IOException, ServletException, AppException {
+	private boolean inputDataIsValid(HttpServletRequest request) throws AppException {
 		boolean isValid = true;
-		// obtain session
 		HttpSession session = request.getSession();
-		// obtain user from session
 		User user = (User) session.getAttribute(REQUEST_USER);
-		// obtain dbManager instance
 		DBManager dbManager = DBManager.getInstance();
-		// obtain captcha from session
-		// init values map
 		Map<String, String> valuesMap = initMap(request);
-		// obtain resource bundre
 		ResourceBundle rb = getResourceBundle(session);
 		Pattern emailPattern = Pattern.compile("^[a-zA-Z][a-zA-Z0-9\\.-]*\\@[a-zA-Z]+\\.[a-zA-Z]+$");
-
-		LOG.trace("Validation starts");
-		// check if fields are filled
 		if (valuesMap.get(REQUEST_FIRST_NAME) == null && valuesMap.get(REQUEST_LAST_NAME) == null
 				&& valuesMap.get(REQUEST_LOGIN) == null && valuesMap.get(REQUEST_EMAIL) == null) {
 			request.setAttribute("fillInMessage", rb.getString("signup.fieldsAreNotFilled"));
 			isValid = false;
 			return isValid;
 		}
-		// first name validation
 		if (valuesMap.get(REQUEST_FIRST_NAME) == null || valuesMap.get(REQUEST_FIRST_NAME).length() < 1) {
 			request.setAttribute("firstNameMessage", rb.getString("signup.firstNameTooShort"));
 			isValid = false;
@@ -137,7 +101,6 @@ public class UpdateProfileCommand extends Command {
 		} else {
 			request.setAttribute(REQUEST_FIRST_NAME, valuesMap.get(REQUEST_FIRST_NAME));
 		}
-		// last name validation
 		if (valuesMap.get(REQUEST_LAST_NAME) == null || valuesMap.get(REQUEST_LAST_NAME).length() < 1) {
 			request.setAttribute("lastNameMessage", rb.getString("signup.lastNameTooShort"));
 			isValid = false;
@@ -147,7 +110,6 @@ public class UpdateProfileCommand extends Command {
 		} else {
 			request.setAttribute(REQUEST_LAST_NAME, valuesMap.get(REQUEST_LAST_NAME));
 		}
-		// login validation
 		String loginMessage = "loginMessage";
 		if (valuesMap.get(REQUEST_LOGIN) == null || valuesMap.get(REQUEST_LOGIN).length() < 3) {
 			request.setAttribute(loginMessage, rb.getString("signup.loginTooShort"));
@@ -164,7 +126,6 @@ public class UpdateProfileCommand extends Command {
 		} else {
 			request.setAttribute(REQUEST_LOGIN, valuesMap.get(REQUEST_LOGIN));
 		}
-		// email validation
 		String emailMessage = "emailMessage";
 		if (valuesMap.get(REQUEST_EMAIL) == null || !emailPattern.matcher(valuesMap.get(REQUEST_EMAIL)).matches()) {
 			request.setAttribute(emailMessage, rb.getString("signup.emailIsNotValid"));
@@ -184,7 +145,6 @@ public class UpdateProfileCommand extends Command {
 		} else {
 			request.setAttribute(REQUEST_EMAIL, valuesMap.get(REQUEST_EMAIL));
 		}
-		// old password validation
 		if (valuesMap.get("oldPassword1Hash") == null || valuesMap.get("oldPassword2Hash") == null) {
 			request.setAttribute("oldPasswordMessage", rb.getString("updateProfile.oldPasswordIsEmpty"));
 			isValid = false;
@@ -193,9 +153,7 @@ public class UpdateProfileCommand extends Command {
 			request.setAttribute("oldPasswordMessage", rb.getString("updateProfile.oldPasswordIsNotCorrect"));
 			isValid = false;
 		}
-		// new password validation
 		if (valuesMap.get(REQUEST_NEW_PASSWORD_1) == null || valuesMap.get(REQUEST_NEW_PASSWORD_1).length() == 0) {
-			// do nothing
 		} else if (valuesMap.get(REQUEST_NEW_PASSWORD_1).length() < 5) {
 			request.setAttribute("passwordMessage", rb.getString("updateProfile.newPasswordTooShort"));
 			isValid = false;
@@ -207,12 +165,11 @@ public class UpdateProfileCommand extends Command {
 			request.setAttribute("passwordMessage", rb.getString("updateProfile.newPasswordsAreNotEqual"));
 			isValid = false;
 		}
-		LOG.trace("Validation end");
 		return isValid;
 	}
 
 	private ResourceBundle getResourceBundle(HttpSession session) {
-		ResourceBundle rb = null;
+		ResourceBundle rb;
 		String currentLocale = (String) session.getAttribute("currentLocale");
 		if (currentLocale == null) {
 			rb = ResourceBundle.getBundle("resources", Locale.getDefault());
@@ -229,7 +186,7 @@ public class UpdateProfileCommand extends Command {
 	 * @return Map<String, String> with request values
 	 */
 	private Map<String, String> initMap(ServletRequest request) {
-		Map<String, String> hashMap = new HashMap<String, String>();
+		Map<String, String> hashMap = new HashMap<>();
 		HttpSession session = ((HttpServletRequest) request).getSession();
 		User user = (User) session.getAttribute(REQUEST_USER);
 		hashMap.put(REQUEST_FIRST_NAME, request.getParameter(REQUEST_FIRST_NAME));
@@ -244,10 +201,8 @@ public class UpdateProfileCommand extends Command {
 			try {
 				hashMap.put("oldPassword1Hash", Password.hash(oldPassword1));
 				hashMap.put("oldPassword2Hash", Password.hash(oldPassword2));
-			} catch (NoSuchAlgorithmException e1) {
-				LOG.error(e1.getMessage());
-			} catch (UnsupportedEncodingException e1) {
-				LOG.error(e1.getMessage());
+			} catch (NoSuchAlgorithmException | UnsupportedEncodingException e1) {
+				LOG.error(e1.getMessage(), e1);
 			}
 		}
 		hashMap.put("currentPassword", user.getPassword());
--- src/main/java/ua/khai/slynko/library/web/command/common/ViewSettingsCommand.java
+++ src/main/java/ua/khai/slynko/library/web/command/common/ViewSettingsCommand.java
@@ -3,8 +3,6 @@ package ua.khai.slynko.library.web.command.common;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.log4j.Logger;
-
 import ua.khai.slynko.library.Path;
 import ua.khai.slynko.library.web.abstractCommand.Command;
 
@@ -16,14 +14,8 @@ import ua.khai.slynko.library.web.abstractCommand.Command;
  */
 public class ViewSettingsCommand extends Command {
 
-	private static final long serialVersionUID = -3071536593627692473L;
-
-	private static final Logger LOG = Logger.getLogger(ViewSettingsCommand.class);
-
 	@Override
 	public String execute(HttpServletRequest request, HttpServletResponse response) {
-		LOG.debug("Command starts");
-		LOG.debug("Command finished");
 		return Path.PAGE_SETTINGS;
 	}
 
