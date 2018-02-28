d6474277e69e42e515615cae5dbe14796abce5d2
Aleksandr Slynko <oleksandr.o.slynko@gmail.com>
2018-02-28 15:32:16 +0200

    ConfirmRequestCommand refactoring

--- src/main/java/ua/khai/slynko/library/web/command/librarian/ConfirmRequestCommand.java
+++ src/main/java/ua/khai/slynko/library/web/command/librarian/ConfirmRequestCommand.java
@@ -1,25 +1,19 @@
 package ua.khai.slynko.library.web.command.librarian;
 
-import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Collections;
 import java.util.Date;
-import java.util.List;
 import java.util.Locale;
 import java.util.ResourceBundle;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-
 import org.apache.commons.lang3.StringUtils;
 import org.apache.log4j.Logger;
-
 import ua.khai.slynko.library.Path;
 import ua.khai.slynko.library.db.DBManager;
 import ua.khai.slynko.library.db.Status;
@@ -35,71 +29,39 @@ import ua.khai.slynko.library.web.abstractCommand.Command;
  */
 public class ConfirmRequestCommand extends Command {
 
-	private static final long serialVersionUID = -3071536593627692473L;
-
 	private static final Logger LOG = Logger.getLogger(ConfirmRequestCommand.class);
 
 	@Override
 	public String execute(HttpServletRequest request, HttpServletResponse response)
-			throws IOException, ServletException, AppException {
-		LOG.debug("Command starts");
-		// obtain session from request
+			throws AppException {
 		HttpSession session = request.getSession();
-		// obtain dbManager instance
 		DBManager dbManager = DBManager.getInstance();
-		// obtain request bean from session
 		CatalogItemRequestBean catalogItemRequestBean = (CatalogItemRequestBean) session
 				.getAttribute("catalogItemRequestBean");
-		// init address
-		String address = Path.PAGE_ERROR_PAGE;
-		// init confirmation type
+		String address;
 		String confirmationType = request.getParameter("bookStatus");
 		String deleteRequest = request.getParameter("deleteRequest");
-		// obtain catalog item id from bean
 		Long id = catalogItemRequestBean.getId();
-		LOG.trace("deleteRequest --> " + deleteRequest);
-		LOG.trace("Forward address --> " + address);
 
 		if (deleteRequest != null && deleteRequest.equals("true")) {
-			// init items list to delete
-			List<String> idList = new ArrayList<String>();
-			idList.add(id.toString());
-			// remove items from db
-			dbManager.removeLibraryCardItemById(idList);
-			// set send redirect into request
+			dbManager.removeLibraryCardItemById(Collections.singletonList(id.toString()));
 			request.setAttribute("sendRedirect", true);
-			// init redirect address
+			request.getSession().setAttribute("requestIsDeletedSuccessfully", true);
 			address = Path.PAGE_HOME_REDERECT;
-			LOG.trace("Redirect address --> " + address);
-			// set request is deleted successfully into session
-			session.setAttribute("requestIsDeletedSuccessfully", true);
 		} else {
-			// check if input data is valid
 			if (!inputDataIsValid(request)) {
 				address = Path.PAGE_CONFIRM_REQUEST_FORM;
-				LOG.trace("Forward address --> " + address);
 			} else {
-				// init date from and date to from request
 				String dateToStr = request.getParameter("dateTo");
 				SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
 				Date dateFrom = new Date();
-				Date dateTo = null;
+				Date dateTo;
 				try {
 					dateTo = sdf.parse(dateToStr);
 				} catch (ParseException e) {
-					LOG.trace("Date parse error");
 					dateTo = new Date();
 				}
-				// init penalty size
-				Integer penaltySize = (Integer) Integer.parseInt(request.getParameter("penaltySize"));
-				LOG.info("id: " + id + ", dateFrom: " + dateFrom + ", dateTo: " + dateTo + ", penaltySize: "
-						+ penaltySize + " are going to be inserted into database");
-				
-				
-				LOG.trace("userEmail ==>" + catalogItemRequestBean.getUserEmail());
-				
-				
-				// update library card item by confirmation type
+				Integer penaltySize = Integer.parseInt(request.getParameter("penaltySize"));
 				if (confirmationType.equals("libraryCard")) {
 					dbManager.updateCatalogItemRequestDateFromDateToPenaltySizeById(dateFrom, dateTo, penaltySize,
 							Status.LIBRARY_CARD.getValue(), id, true, catalogItemRequestBean.getUserEmail());
@@ -107,26 +69,17 @@ public class ConfirmRequestCommand extends Command {
 					dbManager.updateCatalogItemRequestDateFromDateToPenaltySizeById(dateFrom, dateTo, penaltySize,
 							Status.READING_ROOM.getValue(), id, false, catalogItemRequestBean.getUserEmail());
 				}
-				
-				LOG.trace("Insertion is successful ");
-				// set send redirect true into request
 				request.setAttribute("sendRedirect", true);
-				// init redirect address
+				request.getSession().setAttribute("requestIsConfirmedSuccessfully", true);
 				address = Path.PAGE_HOME_REDERECT;
-				LOG.trace("Redirect address --> " + address);
-				// set request is confirmed successfully into session
-				session.setAttribute("requestIsConfirmedSuccessfully", true);
 			}
 		}
-		LOG.debug("Command finished");
 		return address;
 	}
 
-	private boolean inputDataIsValid(HttpServletRequest request) throws IOException, ServletException, AppException {
+	private boolean inputDataIsValid(HttpServletRequest request) throws AppException {
 		boolean isValid = true;
-		// obtain session
 		HttpSession session = request.getSession();
-		// init params for validation
 		String dateToLiteral = "dateTo";
 		String confirmationType = request.getParameter("bookStatus");
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
@@ -141,9 +94,7 @@ public class ConfirmRequestCommand extends Command {
 
 		Matcher dateToMatcher = dateToRegexp.matcher(dateTo);
 
-		// obtain resource bundle by current locale
 		String currentLocale = (String) session.getAttribute("currentLocale");
-		LOG.debug("Locale --> " + currentLocale);
 		ResourceBundle rb;
 		if (currentLocale == null) {
 			rb = ResourceBundle.getBundle("resources", Locale.getDefault());
@@ -151,12 +102,10 @@ public class ConfirmRequestCommand extends Command {
 			rb = ResourceBundle.getBundle("resources", new Locale(currentLocale));
 		}
 
-		LOG.trace("Validation starts");
 		if (DBManager.getInstance().getCatalogItemInstancesByLibraryCardId(id) == 0) {
 			request.setAttribute("validationMessage", rb.getString("confirmRequest.noSuchBook"));
 			isValid = false;
 		}
-		// date to validation
 		if (!dateToMatcher.matches()) {
 			request.setAttribute("dateToMessage", rb.getString("confirmRequest.dateToIsNotCorrect"));
 			isValid = false;
@@ -167,7 +116,6 @@ public class ConfirmRequestCommand extends Command {
 			} catch (ParseException e) {
 				LOG.trace("Date parse error");
 			}
-			// validate date to depending on confirmation type
 			if (confirmationType.equals("libraryCard")) {
 				if (dateToDate.before(new Date())) {
 					request.setAttribute("dateToMessage", rb.getString("confirmRequest.dateToIsInThePast"));
@@ -184,14 +132,12 @@ public class ConfirmRequestCommand extends Command {
 				}
 			}
 		}
-		// penalty size validation
 		if (!StringUtils.isNumeric(penaltySize)) {
 			request.setAttribute("penaltySizeMessage", rb.getString("confirmRequest.penaltySizeIsNotCorrect"));
 			isValid = false;
 		} else {
 			request.setAttribute("penaltySize", penaltySize);
 		}
-		LOG.trace("Validation ends");
 		return isValid;
 	}
 
