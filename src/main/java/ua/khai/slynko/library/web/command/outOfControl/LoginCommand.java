80f3af7ce2ee779da6c5636d9f66c458836c313c
Aleksandr Slynko <oleksandr.o.slynko@gmail.com>
2018-03-29 17:17:40 +0300

    refactoring of LoginCommand

--- src/main/java/ua/khai/slynko/library/web/command/outOfControl/LoginCommand.java
+++ src/main/java/ua/khai/slynko/library/web/command/outOfControl/LoginCommand.java
@@ -1,16 +1,11 @@
 package ua.khai.slynko.library.web.command.outOfControl;
 
-import java.io.IOException;
-import java.security.NoSuchAlgorithmException;
-
-import javax.servlet.ServletException;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import org.apache.log4j.Logger;
-
+import org.apache.commons.lang3.StringUtils;
 import ua.khai.slynko.library.db.DBManager;
 import ua.khai.slynko.library.db.Role;
 import ua.khai.slynko.library.db.entity.User;
@@ -26,74 +21,35 @@ import ua.khai.slynko.library.web.abstractCommand.Command;
  */
 public class LoginCommand extends Command {
 
-	private static final long serialVersionUID = -3071536593627692473L;
-
-	private static final Logger LOG = Logger.getLogger(LoginCommand.class);
-
 	@Override
 	public String execute(HttpServletRequest request, HttpServletResponse response)
-			throws IOException, ServletException, AppException {
-		LOG.debug("Command starts");
-
-		HttpSession session = request.getSession();
+			throws AppException {
 		DBManager manager = DBManager.getInstance();
-
-		// obtain login and password from a request
 		String login = request.getParameter("login");
 		String rememberMe = request.getParameter("rememberMe");
-
-		LOG.trace("Request parameter: loging --> " + login);
-		LOG.trace("Request parameter: rememberMe --> " + rememberMe);
-
-		// obtain password hash
 		String password = Password.hash(request.getParameter("password"));
-
-		// check if login/password are not empty
-		if (login == null || password == null || login.isEmpty() || password.isEmpty()) {
+		if (StringUtils.isEmpty(login) || StringUtils.isEmpty(password)) {
 			throw new AppException("Login/password cannot be empty");
 		}
 
-		// find user in db
 		User user = manager.findUserByLogin(login);
-		LOG.trace("Found in DB: user --> " + user);
-
-		// check if user was not found
 		if (user == null || !password.equals(user.getPassword())) {
 			throw new AppException("Cannot find user with such login/password");
 		}
 
-		// check if user is not blocked
 		boolean userIdBlocked = manager.isUserBlocked(user.getId());
-		LOG.trace("Is user blocked --> " + userIdBlocked);
 		if (userIdBlocked) {
 			throw new AppException("User is blocked. Please contact your system administrator.");
 		}
 
-		// obtain user role
 		Role userRole = Role.getRole(user);
-		LOG.trace("userRole --> " + userRole);
-
-		// set user in session
-		session.setAttribute("user", user);
-		LOG.trace("Set the session attribute: user --> " + user);
-
-		// set user role in session
-		session.setAttribute("userRole", userRole);
-		LOG.trace("Set the session attribute: userRole --> " + userRole);
-
-		// set cookie if remember me is true
+		request.getSession().setAttribute("user", user);
+		request.getSession().setAttribute("userRole", userRole);
 		if (rememberMe != null && rememberMe.equals("true")) {
 			Cookie c = new Cookie("userId", user.getId().toString());
 			c.setMaxAge(24 * 60 * 60);
 			response.addCookie(c);
 		}
-		LOG.info("User " + user + " logged as " + userRole.toString().toLowerCase());
-
-		// get user locale and set locale
-		String address = "changeLocale.jsp?locale=" + user.getLocale();
-		LOG.trace("Set forward address --> " + address);
-
-		LOG.debug("Command finished");
-		return address;
+		return "changeLocale.jsp?locale=" + user.getLocale();
 	}
 }
