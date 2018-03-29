95cc4e462471c59d4fdea6dc45fa5a5f6bea7c82
Aleksandr Slynko <oleksandr.o.slynko@gmail.com>
2018-03-29 17:30:48 +0300

    refactoring of CommandAccessFilter #3

--- src/main/java/ua/khai/slynko/library/web/filter/CommandAccessFilter.java
+++ src/main/java/ua/khai/slynko/library/web/filter/CommandAccessFilter.java
@@ -30,31 +30,21 @@ import ua.khai.slynko.library.db.entity.User;
  */
 public class CommandAccessFilter implements Filter {
 	
-	private static final Logger LOG = Logger.getLogger(CommandAccessFilter.class);
-
-	// commands access	
-	private Map<Role, List<String>> accessMap = new HashMap<Role, List<String>>();
-	private List<String> commons = new ArrayList<String>();	
-	private List<String> outOfControl = new ArrayList<String>();
+	// commands access
+	private Map<Role, List<String>> accessMap = new HashMap<>();
+	private List<String> commons = new ArrayList<>();
+	private List<String> outOfControl = new ArrayList<>();
 	
 	public void destroy() {
-		LOG.debug("Filter destruction starts");
 		// do nothing
-		LOG.debug("Filter destruction finished");
 	}
 
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-		LOG.debug("Filter starts");
-		
 		if (accessAllowed(request)) {
-			LOG.debug("Filter finished");
 			chain.doFilter(request, response);
 		} else {
 			String errorMessasge = "You do not have permission to access the requested resource";
-			
 			request.setAttribute("errorMessage", errorMessasge);
-			LOG.trace("Set the request attribute: errorMessage --> " + errorMessasge);
-			
 			request.getRequestDispatcher(Path.PAGE_ERROR_PAGE)
 					.forward(request, response);
 		}
@@ -63,7 +53,6 @@ public class CommandAccessFilter implements Filter {
 	private boolean accessAllowed(ServletRequest request) {
 			HttpServletRequest httpRequest = (HttpServletRequest) request;
 
-			
 		String commandName = request.getParameter("command");
 		if (commandName == null || commandName.isEmpty()) {
 			return false;
@@ -92,24 +81,17 @@ public class CommandAccessFilter implements Filter {
 				|| commons.contains(commandName);
 	}
 
-	public void init(FilterConfig fConfig) throws ServletException {
-		LOG.debug("Filter initialization starts");
-		
+	public void init(FilterConfig fConfig) {
 		// roles
 		accessMap.put(Role.ADMIN, asList(fConfig.getInitParameter("admin")));
 		accessMap.put(Role.READER, asList(fConfig.getInitParameter("reader")));
 		accessMap.put(Role.LIBRARIAN, asList(fConfig.getInitParameter("librarian")));
-		LOG.trace("Access map --> " + accessMap);
 
 		// commons
 		commons = asList(fConfig.getInitParameter("common"));
-		LOG.trace("Common commands --> " + commons);
 
 		// out of control
 		outOfControl = asList(fConfig.getInitParameter("out-of-control"));
-		LOG.trace("Out of control commands --> " + outOfControl);
-		
-		LOG.debug("Filter initialization finished");
 	}
 	
 	/**
@@ -120,7 +102,7 @@ public class CommandAccessFilter implements Filter {
 	 * @return list of parameter values.
 	 */
 	private List<String> asList(String str) {
-		List<String> list = new ArrayList<String>();
+		List<String> list = new ArrayList<>();
 		StringTokenizer st = new StringTokenizer(str);
 		while (st.hasMoreTokens()) {
 			list.add(st.nextToken());
