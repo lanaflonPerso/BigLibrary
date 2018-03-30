e4af0db645a70ee0764cd5f0cd83345a453e8bc8
Aleksandr Slynko <oleksandr.o.slynko@gmail.com>
2018-03-30 12:57:38 +0300

    refactoring of ListReadersRequestsCommand #3

--- src/main/java/ua/khai/slynko/library/web/command/librarian/ListReadersRequestsCommand.java
+++ src/main/java/ua/khai/slynko/library/web/command/librarian/ListReadersRequestsCommand.java
@@ -5,13 +5,13 @@ import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import ua.khai.slynko.library.Path;
 import ua.khai.slynko.library.db.DBManager;
 import ua.khai.slynko.library.db.Status;
 import ua.khai.slynko.library.db.bean.CatalogItemRequestBean;
 import ua.khai.slynko.library.exception.AppException;
+import ua.khai.slynko.library.exception.DBException;
 import ua.khai.slynko.library.web.abstractCommand.Command;
 
 /**
@@ -25,35 +25,34 @@ public class ListReadersRequestsCommand extends Command {
 	@Override
 	public String execute(HttpServletRequest request, HttpServletResponse response)
 			throws AppException {
-		HttpSession session = request.getSession();
-		String address = Path.PAGE_LIST_READERS_REQUESTS;
-		String beanId = request.getParameter("beanId");
-
-		if (beanId != null) {
-			CatalogItemRequestBean catalogItemRequestBean = null;
-			List<CatalogItemRequestBean> catalogItemRequestsList = (List<CatalogItemRequestBean>) session
-					.getAttribute("catalogItemRequestsList");
-			for (CatalogItemRequestBean cirbListItem : catalogItemRequestsList) {
-				if (cirbListItem.getId() == Long.parseLong(beanId)) {
-					catalogItemRequestBean = cirbListItem;
-				}
-			}
-
-			if (catalogItemRequestBean == null) {
-				catalogItemRequestBean = (CatalogItemRequestBean) session.getAttribute("catalogItemRequestBean");
-			}
-			session.setAttribute("catalogItemRequestBean", catalogItemRequestBean);
-			address = Path.PAGE_CONFIRM_REQUEST_FORM;
+		if (isOpenRequestCommand(request)) {
+			findCatalogItemRequestBean(request);
+			return Path.PAGE_CONFIRM_REQUEST_FORM;
 		} else {
-			List<CatalogItemRequestBean> catalogItemRequestsList = DBManager.getInstance()
-					.findCatalogItemRequests(Status.NOT_CONFIRMED.getValue());
-			if (catalogItemRequestsList == null || catalogItemRequestsList.size() == 0) {
-				request.setAttribute("noMatchesFound", true);
-			} else {
-				catalogItemRequestsList.sort(Comparator.comparing(CatalogItemRequestBean::getTitle));
-				session.setAttribute("catalogItemRequestsList", catalogItemRequestsList);
-			}
+			findCatalogItemRequestsAndSort(request);
+			return Path.PAGE_LIST_READERS_REQUESTS;
 		}
-		return address;
+	}
+
+	private boolean isOpenRequestCommand(HttpServletRequest request) {
+		return request.getParameter("beanId") != null;
+	}
+
+	private void findCatalogItemRequestsAndSort(HttpServletRequest request) throws DBException {
+		List<CatalogItemRequestBean> catalogItemRequestsList = DBManager.getInstance()
+				.findCatalogItemRequests(Status.NOT_CONFIRMED.getValue());
+		if (catalogItemRequestsList == null || catalogItemRequestsList.size() == 0) {
+			request.setAttribute("noMatchesFound", true);
+		} else {
+			catalogItemRequestsList.sort(Comparator.comparing(CatalogItemRequestBean::getTitle));
+			request.getSession().setAttribute("catalogItemRequestsList", catalogItemRequestsList);
+		}
+	}
+
+	public void findCatalogItemRequestBean(HttpServletRequest request) {
+		((List<CatalogItemRequestBean>) request.getSession().getAttribute("catalogItemRequestsList")).stream()
+				.filter(cirbListItem -> cirbListItem.getId() == Long.parseLong(request.getParameter("beanId")))
+				.findFirst()
+				.ifPresent(cirbListItem -> request.getSession().setAttribute("catalogItemRequestBean", cirbListItem));
 	}
 }
